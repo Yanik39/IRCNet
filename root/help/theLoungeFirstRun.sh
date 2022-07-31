7d95530f215122bbc16be38a4ddad01604dfaf12
Yanik39 <12784047+Yanik39@users.noreply.github.com>
2022-08-01 00:53:53 +0200

    Update theLoungeFirstRun.sh

--- root/help/theLoungeFirstRun.sh
+++ root/help/theLoungeFirstRun.sh
@@ -10,3 +10,7 @@ hashPassword=$(/usr/bin/mkpasswd -m bcrypt -s $theLounge_PASSWORD)
         "log": true
 }
 EOF
+
+sed -i 's/oper_user_replace/$theLounge_OPER_USER/g' /home/ircnet/unrealircd/conf/unrelaircd.conf
+sed -i 's/oper_pass_replace/$theLounge_OPER_PASSWORD/g' /home/ircnet/unrealircd/conf/unrelaircd.conf
+sed -i 's/oper_port_replace/$theLounge_PORT/g' /home/ircnet/unrealircd/conf/unrelaircd.conf
